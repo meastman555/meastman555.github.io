cffa9edafd187ef6258e3385bdacc69a7a573e16
Mason Eastman <masoneastman555@gmail.com>
2021-01-13 02:18:14 -0600

    edits to carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,8 @@
 //carousel code adapted from W3 schools at https://www.w3schools.com/howto/howto_js_slideshow.asp
 
+images = document.getElementsByClassName("carousel_image");
+numbers = document.getElementById("carousel_numbers");
+
 //page starts by showing the first image
 //use 1 based indexxing since the image "number" shown starts at 1
 var index = 1;
@@ -12,21 +15,23 @@ function cycle_image(n) {
 }
 
 //manipulates the carousel to show the correct image
+//n is the number (1 based indexxing) image we want to show
 function show_image(n) {
-    var slides = document.getElementsByClassName("carousel_slide");
-    //wraps from end to beginning if needed
-    index = (n > slides.length) ? 1 : index;
-    //wraps from beginning to end if needed
-    index = (n < 1) ? slides.length : index;
+    //wraps the index if it will go out of bounds
+    index = (n > images.length) ? 1 : index;
+    index = (n < 1) ? images.length : index;
     //loops through the images, hiding all the ones except the correct one
     //on the correct one, update the slide number too
-    for(i = 0; i < slides.length; ++i) {
+    for(i = 0; i < images.length; ++i) {
         if(index - 1 == i) {
-            slides[i].style.display = "block";
-            document.getElementById("carousel_numbers").innerHTML = index + "/" + slides.length;
+            images[i].style.display = "block";
+            numbers.innerHTML = index + "/" + images.length;
+            //uses CSS transition to have the numbers fade in, then fades them out here
+            numbers.style.opacity = "1";
+            setTimeout(function(){numbers.style.opacity = "0"}, 1000);
         }
         else {
-            slides[i].style.display = "none";
+            images[i].style.display = "none";
         }
     }
 }
